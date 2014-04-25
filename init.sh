fd28ed0fb7f4c798ce935f607094b1d34fe4fdd9
Kevin Rich <whoiskevinrich@gmail.com>
2014-04-25 11:07:49 -0700

    init.sh will now start rabbitmq, celery, then run the httpd server without the need to execute multiple scripts.

--- init.sh
+++ init.sh
@@ -3,9 +3,17 @@ source /home/vagrant/virtualenvs/sharkeyes/bin/activate
 
 pypath="/home/vagrant/virtualenvs/sharkeyes/bin/python"
 
-$pypath -u /vagrant/manage.py syncdb
-$pypath -u /vagrant/manage.py migrate djcelery
-$pypath -u /vagrant/manage.py migrate pl_plot
-$pypath -u /vagrant/manage.py migrate pl_download
+${pypath} -u /vagrant/manage.py syncdb
+${pypath} -u /vagrant/manage.py migrate djcelery
+${pypath} -u /vagrant/manage.py migrate pl_plot
+${pypath} -u /vagrant/manage.py migrate pl_download
 
 sudo rabbitmq-server -detatched &
+sleep 3
+echo "Waiting 3 seconds for rabbitmq to start"
+
+${pypath} -u /vagrant/manage.py celery worker --loglevel=INFO &
+sleep 3
+echo "Waiting 3 seconds for celery to start"
+
+sudo sh ./runserver.sh
