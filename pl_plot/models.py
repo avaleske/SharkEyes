20ac1de846890d1ede9d63aba1ab48dcf0a386ab
Austin Valeske <avaleske@gmail.com>
2014-05-11 00:22:53 -0700

    adding functions to plotter to get the time out of the file for different ocean_time indices

--- pl_plot/models.py
+++ pl_plot/models.py
@@ -27,22 +27,14 @@ class OverlayManager(models.Manager):
         return newest_overlays.filter(tile_dir__isnull=True).values_list('id', flat=True)
 
     @classmethod
-    def make_all_base_plots(cls):
-        task_list = [make_plot.s(od_id) for od_id in cls.get_all_base_definition_ids()]
-        job = group(task_list)
-        results = job.apply_async()  # this might just be returning results from the first task in each chain
-        return results
-
-    @classmethod
     def get_next_few_days_of_tiled_overlays(cls):
         # starts with "current" overlay, which is the closest to now, forward or backwards, and goes forward 3 days or
         # however far we have data
+        # here assuming that the primary keys for the overlays are only monotonically increasing
+        # and that the newer one is better.
 
         next_few_days_of_overlays = Overlay.objects.filter(applies_at_datetime__gte=timezone.now()-timedelta(hours=2))
         that_are_tiled = next_few_days_of_overlays.filter(tile_dir__isnull=False)
-        # here assuming that the primary keys for the overlays are only monotonically increasing
-        # and that the newer one is better. I suppose that's not always the case.
-        # todo add knowledge about source file to Overlay
         and_the_newest_for_each = that_are_tiled.values('definition', 'applies_at_datetime').annotate(newest_id=Max('id'))
         ids_of_these = and_the_newest_for_each.values_list('newest_id', flat=True)
         # (Yay lazy evaluation...)
@@ -50,25 +42,37 @@ class OverlayManager(models.Manager):
         overlays_to_display = Overlay.objects.filter(id__in=ids_of_these)
         return overlays_to_display
 
+    @classmethod
+    def make_all_base_plots(cls):
+        task_list = [cls.make_plot.s(od_id) for od_id in cls.get_all_base_definition_ids()]
+        job = group(task_list)
+        results = job.apply_async()  # this might just be returning results from the first task in each chain
+        return results
 
-@shared_task(name='pl_plot.make_plot')
-def make_plot(overlay_definition_id):
-    # this just grabs the most recent file. Should the file be tied to the overlay model?
-    datafile = DataFile.objects.latest('model_date')
-    plotter = Plotter(datafile.file.name)
-    overlay_definition = OverlayDefinition.objects.get(pk=overlay_definition_id)
-    plot_filename, key_filename = plotter.make_plot(getattr(plot_functions, overlay_definition.function_name))
+    @shared_task(name='pl_plot.make_plot')
+    def make_plot(self, overlay_definition_id):
+        datafile = DataFile.objects.latest('model_date')
+        plotter = Plotter(datafile.file.name)
+        overlay_definition = OverlayDefinition.objects.get(pk=overlay_definition_id)
+        plot_filename, key_filename = plotter.make_plot(getattr(plot_functions, overlay_definition.function_name))
 
-    overlay = Overlay(
-        file=os.path.join(settings.UNCHOPPED_STORAGE_DIR, plot_filename),
-        key=os.path.join(settings.KEY_STORAGE_DIR, key_filename),
-        created_datetime=timezone.now(),
-        definition_id=overlay_definition_id,
-        # we're grabbing the one for 4 am for now. Assuming utc...
-        applies_at_datetime=timezone.make_aware(datetime.combine(datafile.model_date, time(4)), timezone.utc),
-    )
-    overlay.save()
-    return overlay.id
+        overlay = Overlay(
+            file=os.path.join(settings.UNCHOPPED_STORAGE_DIR, plot_filename),
+            key=os.path.join(settings.KEY_STORAGE_DIR, key_filename),
+            created_datetime=timezone.now(),
+            definition_id=overlay_definition_id,
+            # we're grabbing the one for 4 am for now. also, the 6th time is midnight of the next day
+            applies_at_datetime=timezone.make_aware(
+                datetime.combine(datafile.model_date, plotter.get_time_at_oceantime_index(0)),
+                timezone.utc),
+        )
+        overlay.save()
+        return overlay.id
+
+    # okay, so we need to plot all the base plots, for all the hours, in all the future files.
+    # do we want to have the plotter just take a file and then figure out how to make all the plots for that file? Then
+    # it'd have to be responsible for spinning off the tasks... That's a lot of things for the plotter to do.
+    # also remember that the last index of a file is midnight the next night.
 
 
 class OverlayDefinition(models.Model):
