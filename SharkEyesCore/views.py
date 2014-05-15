4b50d97cf0d8fccc09bfb3007d5835a861920830
Ryan Phillips <wordsandcolor@gmail.com>
2014-05-15 09:11:38 -0700

    a few quick fixes (see description)
    + times in date time picker are now local
    + added ‘fast click’ for better mobile experience
    + made key-transpose responsive

--- SharkEyesCore/views.py
+++ SharkEyesCore/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from pl_plot.models import OverlayManager, OverlayDefinition
+from dateutil import tz
 
 def home(request):
     overlays_view_data = OverlayManager.get_next_few_days_of_tiled_overlays()
-    list_of_times = [ i.applies_at_datetime.strftime('%A, %I %p') for i in overlays_view_data]
+
+    datetimes = [ i.applies_at_datetime.astimezone(tz.tzlocal()).strftime('%A, %I %p') for i in overlays_view_data ]
 
     # a complete hack! it just divides a list of all of the times for all the overlays by the number
     # of defs to get a singular list of overlay times
     num_defs = len(OverlayDefinition.objects.all())
-    list_of_times = list_of_times[:len(list_of_times)/num_defs]
+    list_of_times = datetimes[:len(datetimes)/num_defs]
 
     context = {'overlays': overlays_view_data, 'defs': OverlayDefinition.objects.all(), 'times':list_of_times }
     return render(request, 'index.html', context)
