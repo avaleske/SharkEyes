bbd9f6b7a90230c947142053354ac2d268a16441
Austin Valeske <avaleske@gmail.com>
2014-09-23 01:06:10 -0700

    added a lot to fabfile. now install prereqs, python, geo tools, apache, mysql. Still need to configure virtual env install pip packages and basemap, and configure apache, mod_wsgi, and mysql

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from fabric.api import run, env, sudo, local, cd
+from fabric.api import run, env, sudo, local, cd, settings
 from fabric.contrib.console import confirm
 from fabric.contrib.files import exists
 
@@ -24,6 +24,16 @@ def install_prereqs():
     sudo('yum -y install wget zlib-devel bzip2-devel openssl-devel ncurses-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel')
     # sudo('yum -y install centos-release-SCL')         # let's not do this, so we have more control over things
 
+    # repos
+    with settings(warn_only=True):
+        with cd('/opt'):
+            if run('rpm -q epel-release-6-8.noarch').return_code != 0:
+                sudo('wget http://mirror-fpt-telecom.fpt.net/fedora/epel/6/i386/epel-release-6-8.noarch.rpm')
+                sudo('rpm -ivh epel-release-6-8.noarch.rpm')
+            if run('rpm -q elgis-release-6-6_0.noarch').return_code != 0:
+                sudo('wget http://elgis.argeo.org/repos/6/elgis-release-6-6_0.noarch.rpm')
+                sudo('rpm -Uvh elgis-release-6-6_0.noarch.rpm')
+
 
 def install_python27():
     if not exists('/usr/bin/python2.7'):
@@ -37,25 +47,53 @@ def install_python27():
             sudo('ln -s ../../libpython2.7.so.1.0')     # linking so other stuff knows where to find Python
             sudo('ln -s ../../libpython2.7.so')
         sudo('ln -s /usr/local/bin/python2.7 /usr/bin/')    # and adding another link so it's easy to use the right one later
+    sudo('yum -y install python-pip python-devel')          # todo is it a problem that this is python 2.6.6 devel?
+    sudo('pip install --upgrade pip')
+    sudo('pip install virtualenv')
+    sudo('pip install virtualenvwrapper')
+
+
+def install_apache():
+    sudo('yum -y install httpd')
+    sudo('yum -y install httpd-devel.x86_64')
 
 
 def install_mysql():
-    pass
+    sudo('yum -y install mysql')
+    sudo('yum -y mysql-python')
+    sudo('yum -y mysql-server')
 
 
 def install_geo_tools():
-    pass
+    sudo('yum -y install blas-devel freetype-devel lapack-devel libpng-devel')
+    sudo('yum -y install geos-devel proj')
+    sudo('yum -y install rabbitmq-server')
 
+    if not exists('/opt/gdal-1.10.1/install_complete'):
+        with cd('/opt'):
+            sudo('wget http://download.osgeo.org/gdal/1.10.1/gdal-1.10.1.tar.gz')
+            sudo('tar -xvzf gdal-1.10.1.tar.gz')
+        with cd('/opt/gdal-1.10.1/'):
+            sudo('./configure --prefix=/usr/local')
+            sudo('make && make install')
+            sudo('touch install_complete')
+
+    sudo("if grep -Fxq '/usr/local/lib' '/etc/ld.so.conf'; then :; else echo '/usr/local/lib' | tee -a /etc/ld.so.conf; fi")
+    sudo('ldconfig')
 
 def setup_python():
     pass
 
 
-def setup_apache():
+def configure_apache():
     pass
 
 
-def setup_mod_wsgi():
+def configure_mod_wsgi():
+    pass
+
+
+def configure_mysql():
     pass
 
 
@@ -66,12 +104,13 @@ def deploy():
 def provision():
     install_prereqs()
     install_python27()
+    install_apache()
     install_mysql()
     install_geo_tools()
     setup_python()
-    setup_apache()
-    setup_mod_wsgi()
-    deploy()
+    configure_apache()
+    configure_mod_wsgi()
+    configure_mysql()
 
 
 def uname():
