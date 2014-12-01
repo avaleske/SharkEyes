ef4501f13a14f395b7222f9d7b814b0c2048a9dd
maria-powell-osu <weberma@onid.oregonstate.edu>
2014-11-30 16:53:58 -0800

    First Stage of URL Authentication

--- pl_chop/views.py
+++ pl_chop/views.py
@@ -6,8 +6,12 @@ from pl_chop.models import TileManager
 
 
 def test_chop(request):
-    results = TileManager.tile_next_few_days_of_untiled_overlays()
-    if results is None:
-        return HttpResponse("Nothing to tile")
+    #Validate User Permissions
+    if request.user.is_anonymous():
+        return HttpResponse("You do not have admin permissions to this site. Please contact your page administrator.")
     else:
-        return HttpResponse(results.get().__str__())
+        results = TileManager.tile_next_few_days_of_untiled_overlays()
+        if results is None:
+            return HttpResponse("Nothing to tile")
+        else:
+            return HttpResponse(results.get().__str__())
